2b2f3baf722a03cf4836e85566cb16e037fad393
Pablo Liste Cancela <ppabli12@gmail.com>
2025-02-05 19:06:47 +0100

    feat: Histogram first implementation and script generalizations

--- P1_introduccion_algoritmos/histograma.c
+++ P1_introduccion_algoritmos/histograma.c
@@ -1,29 +1,82 @@
 /**
  * @brief Template for Labs
- * 
- * PAE [G4011452] Labs
- * Last update: 
+ *
+ * PAE [G4012452] Labs
+ * Last update: 03/02/2025
  * Issue date:  30/01/2022
- * 
- * Student name: 
+ *
+ * Student name: Jorge Lojo Abal y Pablo Liste Cancela
  *
  */
 
 // General utilities
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
-// Custon utilities (in case of need) 
+// The file loadPGM.h will be used for defining load and export functions
+#include "pgmio.h"
 
-// Implement the exercise in a function here
+#define GRAY_LEVELS 256
 
+void calculateHistogram(unsigned char* xu8, int w, int h, int* hist) {
 
-// Main program
-int main(int argc, char *argv[])
-{ 
-    // start timer
+    // Calculate histogram
+    for (int i = 0; i < w * h; i++) {
 
-    // call the function
+        hist[xu8[i]]++;
 
-    // stop timer
+    }
 
-    return 0;
+}
+
+void printHistogram(int* hist) {
+
+    // Print histogram
+    for (int i = 0; i < GRAY_LEVELS; i++) {
+
+        printf("%d: %d\n", i, hist[i]);
+
+    }
+
+}
+
+int main(int argc, char *argv[]) {
+
+    // Check for arguments
+    if (argc < 2) {
+        printf("Use %s file.pgm\n", argv[0]);
+        exit(-1);
+    }
+
+    // Image size variables (width and height)
+    int w;
+    int h;
+
+    // Load .pgm image
+    unsigned char* xu8 = loadPGMu8(argv[1], &w, &h);
+
+    // Check if the image has been loaded correctly
+    if (xu8 == NULL) {
+
+        printf("Error: Image could not be loaded\n");
+        return EXIT_FAILURE;
+
+    }
+
+    // Histogram array
+    int hist[GRAY_LEVELS] = {0};
+
+    // Compute histogram of the image
+    calculateHistogram(xu8, w, h, hist);
+
+    // Print histogram of the image
+    #ifdef DEBUG
+        printHistogram(hist);
+    #endif
+
+    free(xu8);
+
+    return EXIT_SUCCESS;
+
 }
--- P1_introduccion_algoritmos/script.sh
+++ P1_introduccion_algoritmos/script.sh
@@ -13,8 +13,9 @@ module load gcc
 gcc -O2 -Wall -o main $1
 
 sum_time=0
+max_iter=10
 
-for i in {1..10}
+for i in $(seq 1 $max_iter)
 do
     output=$(./main 2 100000)
     time=$(echo "$output" | grep "PAE | Time:" | awk '{print $4}')
@@ -22,7 +23,7 @@ do
     sum_time=$(echo "$sum_time + $time" | bc)
 done
 
-avg_time=$(echo "scale=6; $sum_time / 10" | bc)
+avg_time=$(echo "scale=6; $sum_time / $max_iter" | bc)
 echo "Total: $sum_time | Average: $avg_time seconds"
 
 rm main
