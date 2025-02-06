520f34463fd681120ddf400ba9324e8a9ca8f3ea
Pablo Liste Cancela <ppabli12@gmail.com>
2025-02-06 11:00:37 +0100

    fix: Minor changes and improvements

--- P1_introduccion_algoritmos/histograma.c
+++ P1_introduccion_algoritmos/histograma.c
@@ -13,12 +13,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
+#include <sys/time.h>
 
 // The file loadPGM.h will be used for defining load and export functions
 #include "pgmio.h"
 
 #define GRAY_LEVELS 256
 
+/**
+ * @brief Calculate the histogram of an image
+ *
+ * @param xu8 Image data
+ * @param w Image width
+ * @param h Image height
+ * @param hist Histogram array
+ */
 void calculateHistogram(unsigned char* xu8, int w, int h, int* hist) {
 
     // Calculate histogram
@@ -30,17 +40,30 @@ void calculateHistogram(unsigned char* xu8, int w, int h, int* hist) {
 
 }
 
+/**
+ * @brief Print the histogram of an image
+ *
+ * @param hist Histogram array
+ */
 void printHistogram(int* hist) {
 
     // Print histogram
     for (int i = 0; i < GRAY_LEVELS; i++) {
 
-        printf("%d: %d\n", i, hist[i]);
+        // Display the number of pixels with intensity i in the image using CSV format
+        printf("%d, %d\n", i, hist[i]);
 
     }
 
 }
 
+/**
+ * @brief Main function
+ *
+ * @param argc Number of arguments
+ * @param argv Arguments
+ * @return int Exit status
+ */
 int main(int argc, char *argv[]) {
 
     // Check for arguments
@@ -48,6 +71,10 @@ int main(int argc, char *argv[]) {
         printf("Use %s file.pgm\n", argv[0]);
         exit(-1);
     }
+
+    struct timeval start;
+    struct timeval start2;
+    struct timeval end;
 
     // Image size variables (width and height)
     int w;
@@ -67,15 +94,30 @@ int main(int argc, char *argv[]) {
     // Histogram array
     int hist[GRAY_LEVELS] = {0};
 
+    // Start timer
+    gettimeofday(&start, NULL);
+    gettimeofday(&start2, NULL);
+
     // Compute histogram of the image
     calculateHistogram(xu8, w, h, hist);
+
+    // Stop timer
+    gettimeofday(&end, NULL);
 
     // Print histogram of the image
     #ifdef DEBUG
         printHistogram(hist);
     #endif
 
+    // Free memory
     free(xu8);
+
+    // Calculate the elapsed time
+    double overhead = (start2.tv_sec - start.tv_sec) + (start2.tv_usec - start.tv_usec) / 1e6;
+    double time = (end.tv_sec - start2.tv_sec) + (end.tv_usec - start2.tv_usec) / 1e6 - overhead;
+
+    // Print the results
+    printf("PAE | Time: %f\n", time);
 
     return EXIT_SUCCESS;
 
