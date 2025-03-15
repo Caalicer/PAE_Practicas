c9d4cd1ea9c7c9f148725e6392b11e10dc40ea55
Pablo Liste Cancela <ppabli12@gmail.com>
2025-03-15 15:40:59 +0100

    feat: Added even more data just por si acaso

--- P2_cuda/lab2/scripts/script_3.sh
+++ P2_cuda/lab2/scripts/script_3.sh
@@ -4,7 +4,7 @@
 #SBATCH -c 32
 #SBATCH --mem 64G
 #SBATCH --gres gpu:a100:1
-#SBATCH -t 00:10:00
+#SBATCH -t 02:00:00
 #SBATCH -J P2_1
 #SBATCH -o ./outs/P2_1.o
 #SBATCH -e ./outs/P2_1.e
@@ -16,13 +16,22 @@ nvcc -O2 -o main_1 $1
 block_sizes=(1 2 4 8 16 32 64 128 256 512 1024)
 
 max_iter=10
+n_values=($(seq 0 5 35))
 
-for block in ${block_sizes[@]}; do
+max_iter=10
 
-	for i in $(seq 1 $max_iter); do
+for n in ${n_values[@]}; do
 
-		output=$(./main_1 $block)
-		echo -e $output
+	n_value=$(( (n * 1024 * 1024 * 1024) / 8 ))
+
+	for block in ${block_sizes[@]}; do
+
+		for i in $(seq 1 $max_iter); do
+
+			output=$(./main_1 $block 2.0 $n_value)
+			echo -e $output
+
+		done
 
 	done
 
